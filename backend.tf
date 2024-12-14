7872e8e155e387087b9a26b2e55bde9da47494d6
marabiocho <103324080+marabiocho@users.noreply.github.com>
2024-12-14 00:13:17 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "kantin10"
+    bucket         = "kantin"
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
     #dynamodb_table = "mrcloudbook-dynamo-db-table"
