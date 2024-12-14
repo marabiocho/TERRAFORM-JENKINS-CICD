44aa6030d702ef682e87abaf1e7948d684ff9ea1
marabiocho <103324080+marabiocho@users.noreply.github.com>
2024-12-14 00:23:09 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-0e2c8caa4b6378d8c"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
